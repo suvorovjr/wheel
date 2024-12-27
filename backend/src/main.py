94f80557ff9654ab611a36b7a62f84a222c29e59
suvorovjr <pglavan1998@mail.ru>
2024-12-27 12:54:01 +0300

    Add changes in CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -25,11 +25,15 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-origins = ["http://127.0.0.1:5500", "http://localhost:5500", "http://nginx"]
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://89.108.121.202",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
